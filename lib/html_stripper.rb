5430ad25754da38448a74baafde61973f3154e24
jkr2255 <jkr2255@users.noreply.github.com>
2016-09-24 18:47:57 +0900

    add basic functionality and test for default behavior

--- lib/html_stripper.rb
+++ lib/html_stripper.rb
@@ -1,5 +1,79 @@
-require "html_stripper/version"
+require 'html_stripper/version'
 
-module HtmlStripper
-  # Your code goes here...
+# Main class for HtmlStripper.
+class HtmlStripper
+  CDATA_PATTERNS = %w(<![CDATA[ ]]>).freeze
+
+  COND_COMMENT_PATTERNS = [/<!--<!\[|<!--\[/, ']-->'].freeze
+
+  DEFAULT_OPTIONS = {
+    strip_comments: true,
+    simplify_lines: true,
+    minify_spaces: true,
+    keep_tags: [:script, :style, :textarea, :pre].freeze,
+    keep_patterns: [CDATA_PATTERNS, COND_COMMENT_PATTERNS].freeze
+  }.freeze
+
+  TAGNAME_END_PATTERN = '[\s/>]'.freeze
+
+  NO_HIT_REGEX = /(?!)/
+
+  NEWLINE_REGEX = /\s*\n\s+/
+
+  SPACES_REGEX = /[ \t]{2,}(?=[^> \t]*<|\z)/
+
+  COMMENT_REGEX = /<!--.*?-->/m
+
+  # Generates HtmlStripper.
+  # @param  [Hash] opts the options for stripping.
+  # @option opts [true, false] :strip_comments
+  #  whether to strip comments.
+  # @option opts [true, false] :simplify_lines
+  #  whether to strip comments.
+  def initialize(opts = {})
+    @options = DEFAULT_OPTIONS.merge(opts)
+    @options[:keep_patterns] = @options[:keep_patterns].map do |row|
+      row.map do |item|
+        item.is_a?(String) ? Regexp.escape(item) : item
+      end
+    end
+    build_regexes
+  end
+
+  # strip from specified HTML.
+  # @param [String] html input HTML.
+  # @return [String] return stripped HTML.
+  def run(html)
+    splitted = html.split @split_regex
+    splitted.map { |frag| process_fragment frag }.join
+  end
+
+  private
+
+  def process_fragment(html)
+    return html if html =~ @exclude_head_regex
+    html = html.gsub(COMMENT_REGEX, '') if @options[:strip_comments]
+    html = html.gsub(NEWLINE_REGEX, "\n") if @options[:simplify_lines]
+    html = html.gsub(SPACES_REGEX, ' ') if @options[:minify_spaces]
+    html
+  end
+
+  def build_tag_patterns
+    @tag_patterns = @options[:keep_tags].map do |sym|
+      ["<#{sym}#{TAGNAME_END_PATTERN}", "</#{sym}>"]
+    end
+  end
+
+  def build_regexes
+    build_tag_patterns
+    patterns = @tag_patterns + @options[:keep_patterns]
+    if patterns.empty?
+      @exclude_head_regex = @split_regex = NO_HIT_REGEX
+      return
+    end
+    @exclude_head_regex =
+      /\A#{patterns.map { |item| item[0] }.join('|')}/mi
+    @split_regex =
+      /#{patterns.map { |item| "(#{item[0]}.*?#{item[1]})" }.join('|')}/mi
+  end
 end
--- spec/html_stripper_spec.rb
+++ spec/html_stripper_spec.rb
@@ -5,7 +5,57 @@ describe HtmlStripper do
     expect(HtmlStripper::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context '.new' do
+    it 'can be initialized with option Hash' do
+      expect { HtmlStripper.new({}) }.not_to raise_error
+    end
+  end
+
+  let(:default_stripper) { HtmlStripper.new }
+
+  context '#run' do
+    context '(with default setting)' do
+      default_tests = [
+        {
+          title: 'strips HTML comment',
+          source: '<test><!-- hoge --><foo /><!----></test>',
+          expected: '<test><foo /></test>'
+        },
+        {
+          title: 'unifies multiple newlines and spaces to one newline',
+          source: "<test>  \n \n </test><test2>\n\n</test2>",
+          expected: "<test>\n</test><test2>\n</test2>"
+        },
+        {
+          title: 'minifies continuous spaces and tabs outside tags to one space',
+          source: "<test>   <test2>\t \t</test2></test>",
+          expected: '<test> <test2> </test2></test>'
+        },
+        {
+          title: "doesn't change continuous spaces inside tags",
+          source: '<test  attr1=" val "  ><test2   /> </test>'
+        },
+        {
+          title: 'keeps anything inside style, script, textarea, pre tags',
+          source: "<style><!-- foo --></style><script async>\n\t\n</script>" \
+                  "<textarea>   </textarea><pre>\t\t\n\n\t\t</pre>"
+        },
+        {
+          title: 'keeps inside CDATA section',
+          source: "<![CDATA[\n\n\t  <!-- saaa -->  ]]>"
+        },
+        {
+          title: 'keeps conditional comments',
+          source: '<!--[if ie]> foo <![endif]--><!--[if !IE]><!--> bar <!--<![endif]-->'
+        }
+      ]
+
+      default_tests.each do |test|
+        it test[:title] do
+          expect(default_stripper.run(test[:source]))
+            .to eq(test[:expected] || test[:source])
+        end
+      end
+    end
   end
 end
